53f79d8a5c991571ef06b0706a92a8644aec8ab9
UnoWu <st9007a@gmail.com>
2022-01-31 23:15:49 +0800

    Config telescope-file-browser

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -9,10 +9,28 @@ telescope.setup {
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                        -- the default case_mode is "smart_case"
-    }
-  },
-  file_browser = {
-    theme = "ivy",
+    },
+    file_browser = {
+      theme = "ivy",
+      mappings = {
+        ["i"] = {
+          ["<C-c>"] = fb_actions.create,
+          ["<C-r>"] = fb_actions.rename,
+          ["<C-d>"] = fb_actions.remove,
+          ["<C-m>"] = fb_actions.move,
+          ["<C-y>"] = fb_actions.copy,
+          ["<C-h>"] = fb_actions.toggle_hidden,
+        },
+        ["n"] = {
+          ["c"] = fb_actions.create,
+          ["r"] = fb_actions.rename,
+          ["d"] = fb_actions.remove,
+          ["m"] = fb_actions.move,
+          ["y"] = fb_actions.copy,
+          ["h"] = fb_actions.toggle_hidden,
+        },
+      },
+    },
   },
 }
 -- To get fzf loaded and working with telescope, you need to call
@@ -23,5 +41,15 @@ telescope.load_extension('file_browser')
 local keymap_options = { noremap = true }
 
 vim.api.nvim_set_keymap('n', '`', '<cmd>Telescope file_browser<CR>', keymap_options)
-vim.api.nvim_set_keymap('n', '<space>f', '<cmd>Telescope find_files<CR>', keymap_options)
-vim.api.nvim_set_keymap('n', '<space>b', '<cmd>Telescope buffers<CR>', keymap_options)
+vim.api.nvim_set_keymap(
+  'n',
+  '<space>f',
+  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_ivy({}))<CR>",
+  keymap_options
+)
+vim.api.nvim_set_keymap(
+  'n',
+  '<space>b',
+  "<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_ivy({}))<CR>",
+  keymap_options
+)
