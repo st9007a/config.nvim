4574dd9bd2397e51b2acdb2a54b911161dbaca0e
UnoWu <st9007a@gmail.com>
2022-01-31 18:50:23 +0800

    Detect platform and cmd sh,json

--- configure.sh
+++ configure.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sed -i '.bak' "s~HOME~$HOME~g" lua/setup/lsp/lsp.lua
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '.bak' "s~HOME~$HOME~g" lua/setup/lsp/lsp.lua
+elif [[ "$OSTYPE" == "linux-gun"* ]]; then
+    sed -i "s~HOME~$HOME~g" lua/setup/lsp/lsp.lua
+else
+    exit 1
+fi
