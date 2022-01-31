8d18f920417faa812af8e76270a34e364ed97234
UnoWu <st9007a@gmail.com>
2022-01-31 20:29:31 +0800

    Fix configure workflow

--- configure.sh
+++ configure.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '.bak' "s~HOME~$HOME~g" lua/setup/lsp/lsp.lua
-elif [[ "$OSTYPE" == "linux-gun"* ]]; then
-    sed -i "s~HOME~$HOME~g" lua/setup/lsp/lsp.lua
-else
-    exit 1
-fi
+sed "s~HOME~$HOME~g" lua/setup/lsp/lsp_template.lua > lua/setup/lsp/lsp.lua
