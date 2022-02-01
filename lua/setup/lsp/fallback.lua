da9696b90b883472b855993ae8e6189ca5dd9049
UnoWu <st9007a@gmail.com>
2022-02-01 22:15:13 +0800

    Trim trailing newline and whitespace in formatting

--- lua/setup/lsp/fallback.lua
+++ lua/setup/lsp/fallback.lua
@@ -2,6 +2,8 @@ local null_ls = require('null-ls')
 
 null_ls.setup {
   sources = {
+    null_ls.builtins.formatting.trim_whitespace,
+    null_ls.builtins.formatting.trim_newlines,
     null_ls.builtins.formatting.isort.with({
       filetypes = {"python"},
     }),
