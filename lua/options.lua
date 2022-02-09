f81309a292e9fa8c89659b19477af3cc1f3b4343
UnoWu <st9007a@gmail.com>
2022-02-09 22:40:33 +0800

    Replace builtin grep with ripgrep if available

--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,8 @@ vim.o.hlsearch = false
 vim.o.inccommand = 'nosplit'
 vim.o.wildignorecase = true
 vim.o.updatetime = 1000
+
+if vim.fn.executable('rg') then
+  vim.o.grepprg = 'rg --no-heading --vimgrep --smart-case'
+  vim.o.grepformat = '%f:%l:%c:%m'
+end
