63119900a4f3563f1aba963fef429b0c63b95d86
UnoWu <st9007a@gmail.com>
2022-04-18 09:51:48 +0800

    Fix completion

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -71,8 +71,6 @@ cmp.setup({
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback()
       end
@@ -82,8 +80,6 @@ cmp.setup({
         cmp.select_prev_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback()
       end
