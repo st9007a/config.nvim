77cef9d6fa37af65a859764027479adc575caa53
UnoWu <st9007a@gmail.com>
2022-04-18 09:27:36 +0800

    Remove file type setting for nvim v0.7.0

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-local version = vim.version()
-
-if (version.minor > 6) then
-  vim.g.do_filetype_lua = 1
-  vim.g.did_load_filetypes = 0
-end
-
-
 require('plugins')
 require('options')
 require('mappings')
