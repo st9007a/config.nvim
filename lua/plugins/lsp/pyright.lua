96aaa9517b57b67caf2c686cf75502f5b94cc1d9
UnoWu <uno@aetherai.com>
2022-05-08 13:38:55 +0800

    Fix pyright config

--- lua/plugins/lsp/pyright.lua
+++ lua/plugins/lsp/pyright.lua
@@ -1,14 +1,8 @@
-local home = os.getenv('HOME')
-
-
 local M = {
   settings = {
     python = {
       analysis = {
-        typeshedPaths = {
-          home .. '/.config/nvim/lib/python/typeshed',
-        },
-        stubPath = home .. '/.config/nvim/lib/python/python-type-stubs',
+        diagnosticMode = 'openFilesOnly',
       },
     },
   },
