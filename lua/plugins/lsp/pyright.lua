ec6282f6758d8455e4188d2c5f2c5f0ba75fffc8
UnoWu <uno@aetherai.com>
2022-05-20 10:07:53 +0800

    Revert "Fix pyright config"
    This reverts commit 96aaa9517b57b67caf2c686cf75502f5b94cc1d9.

--- lua/plugins/lsp/pyright.lua
+++ lua/plugins/lsp/pyright.lua
@@ -1,8 +1,14 @@
+local home = os.getenv('HOME')
+
+
 local M = {
   settings = {
     python = {
       analysis = {
-        diagnosticMode = 'openFilesOnly',
+        typeshedPaths = {
+          home .. '/.config/nvim/lib/python/typeshed',
+        },
+        stubPath = home .. '/.config/nvim/lib/python/python-type-stubs',
       },
     },
   },
